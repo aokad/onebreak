887b6222add1b87a9a6e0e9aa91661ca66f286b0
friend1ws <friend1ws@gmail.com>
2019-05-18 07:56:44 +0000

    modify setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='EBbreak',
       version='0.1.0',
       description='Python tools for various analysis for breakpoints from cancer genome sequencing data.',
-      author='Yuichi Shiraishi',
+      author='Yuichi Shiraishi, Yuki Saito',
       author_email='friend1ws@gamil.com',
       url='https://github.com/friend1ws/bp_utils',
       package_dir = {'': 'lib'},
