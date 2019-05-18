dfcc31983c6e7856aa0ff8135799e52a72a2a9f9
friend1ws <friend1ws@gmail.com>
2019-05-18 08:30:41 +0000

    modify package structure

--- setup.py
+++ setup.py
@@ -1,16 +1,49 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-setup(name='EBbreak',
-      version='0.1.0',
-      description='Python tools for various analysis for breakpoints from cancer genome sequencing data.',
-      author='Yuichi Shiraishi, Yuki Saito',
-      author_email='friend1ws@gamil.com',
-      url='https://github.com/friend1ws/bp_utils',
-      package_dir = {'': 'lib'},
-      packages=['EBbreak'],
-      scripts=['EBbreak'],
-      license='GPL-3'
-     )
+here = path.abspath(path.dirname(__file__))
+
+def get_version():
+    with open(path.join(here, "ebbreak/version.py"), encoding = 'utf-8') as hin:
+        for line in hin:
+            if line.startswith("__version__"):
+                version = line.partition('=')[2]
+                return version.strip().strip('\'"')
+    raise ValueError('Could not find version.')
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'ebbreak',
+    version = get_version(),
+    description='Python tools for various analysis for breakpoints from cancer genome sequencing data.',
+    long_description=long_description, 
+    long_description_content_type='text/markdown',  
+    url = 'https://github.com/nccmo/EBbreak_modified',
+    author = 'Yuichi Shiraishi, Yuki Saito, Kenichi Chiba',
+    author_email = 'friend1ws@gamil.com',
+    license = 'GPLv3',
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
+
+    packages = find_packages(exclude = ['tests']),
+
+    install_requires = [],
+    entry_points = {'console_scripts': ['ebbreak = ebbreak:main']}
+
+)
+
 
